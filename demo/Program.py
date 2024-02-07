b2ce92b44b56ce4878c094fa3664b6dd85a74d95
MSWeiz <134125504+MSWeiz@users.noreply.github.com>
2024-02-08 00:38:53 +0300

    Update Program.py

--- demo/Program.py
+++ demo/Program.py
@@ -1,10 +1,11 @@
-﻿# SDK Pullenti Address, version 4.21, october 2023. Copyright (c) 2013, Pullenti. All rights reserved.
+# SDK Pullenti Address, version 4.21, october 2023. Copyright (c) 2013, Pullenti. All rights reserved.
 # Non-Commercial Freeware and Commercial Software.
 # This class is generated using the converter Unisharping (www.unisharping.ru) from Pullenti C# project.
 # The latest version of the code is available on the site www.pullenti.ru
 
 import typing
 import pathlib
+import xml.etree.ElementTree as ET
 from pullenti.unisharp.Utils import Utils
 
 from pullenti.address.ProcessTextParams import ProcessTextParams
@@ -33,28 +34,22 @@ class Program:
         info = AddressService.get_gar_statistic()
         if (info is not None): 
             print(str(info), flush=True)
-        print("Root Gar Objects:", flush=True)
         # дополнительные параметры анализа
         pars = ProcessTextParams()
-        # дефолтовый регион (или можно явно город как pars.DefaultObject = garMoscow)
-        pars.default_regions.append(77)
-        # анализ текстовых фрагментов с адресами
-        # а вот анализ текста, который содержит ТОЛЬКО АДРЕС, и ничего более (поле ввода адреса, например)
-        text = "Москва, ул. 16-я Парквая д.2 кв.3 и какой-то мусор"
+        text = "Москва, ул. 16-я Парквая 2 3"
         saddr = AddressService.process_single_address_text(text, pars)
-        print("\nAnalyze single address: {0}".format(text), flush=True)
+        print("\nAnalyze address: {0}".format(text), flush=True)
         if (saddr is None): 
             print("Fatal process error", flush=True)
         else: 
             print("Coefficient: {0}".format(saddr.coef), flush=True)
-            if (saddr.error_message is not None): 
-                print("Message: {0}".format(saddr.error_message), flush=True)
-            for item in saddr.items: 
-                print("Item: {0}".format(str(item)), end="", flush=True)
-                if (len(item.gars) > 0): 
-                    for gar in item.gars: 
-                        print(" (Gar: {0}, GUID={1})".format(str(gar), gar.guid), end="", flush=True)
-                print("", flush=True)
+            if saddr.coef > 41:
+                if (saddr.error_message is not None):
+                    print("Message: {0}".format(saddr.error_message), flush=True)
+                for item in saddr.items:
+                    #print("Item: {0}".format(str(item)), end="", flush=True)
+                    #print("", flush=True)
+                    print(item)
 
 if __name__ == "__main__":
     Program.main(None)
